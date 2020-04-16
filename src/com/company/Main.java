e4990014e05f56ebd98064a9c112a3bc7bb7b3c1
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-16 14:23:09 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World\nMy name is Kittiphop\nI'm 15 years old");
+    }
+}
